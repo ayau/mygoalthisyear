43d3ab1e444b27ba8eed09701edd3ab26a3c4619
Alex Yau <ayau@sas.upenn.edu>
2013-01-05 17:02:53 +0800

    db quick fix

--- db/migrate/20121211033327_change_completed_to_boolean_in_goals.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCompletedToBooleanInGoals < ActiveRecord::Migration
-  def up
-    change_column :goals, :completed, :boolean, :default => false
-  end
-
-  def down
-    change_column :goals, :completed, :integer
-  end
-end
