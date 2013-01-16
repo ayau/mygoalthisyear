8812e61baa44b1ec609c662a48196fd087adacd3
Alex Yau <ayau@sas.upenn.edu>
2013-01-16 16:14:42 -0500

    one more try

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     has_many :goals, :through => :commitments,
              :conditions => {:parent_id => 0},
              :select => 'goals.*, commitments.completed_at,
-                        CAST(commitments.completed as float8) as completed,
+                        CAST(commitments.completed as float8),
                         commitments.is_current'
 
     has_many :subgoals, :through => :commitments, :source => :goal,
